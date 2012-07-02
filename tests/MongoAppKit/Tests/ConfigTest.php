68c5fb7fc4cd0e32d99ab5e880f3032728be008c
David Henning <madcat.me@gmail.com>
2012-07-02 22:46:26 +0200

    new tests

--- tests/MongoAppKit/Tests/ConfigTest.php
+++ tests/MongoAppKit/Tests/ConfigTest.php
@@ -42,4 +42,46 @@ class ConfigTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals($expectedValue, $sanitizedValue);
 	}
 
+	public function testSanitizeNull() {
+		$config = new Config();
+		$value = null;
+		$expectedValue = null;
+		$sanitizedValue = $config->sanitize($value);
+
+		$this->assertEquals($expectedValue, $sanitizedValue);
+	}
+
+	public function testAddConfigFile() {
+		$values = array('foo' => 'bar');
+		$fileName = __DIR__ . '/test.json';
+		file_put_contents($fileName, json_encode($values));
+
+		$config = new Config();
+		$config->addConfigFile($fileName);
+		unlink($fileName);
+
+		$this->assertEquals($values, $config->getProperties());
+	}
+
+	public function testAddConfigFileWithEmptyFileName() {
+		try {
+			$config = new Config();
+			$config->addConfigFile(null);
+		} catch(\InvalidArgumentException $e) {
+			return;
+		}
+
+		$this->fail('Expected InvalidArgumentException was not thrown.');
+	}
+
+	public function testAddConfigFileWithWrongFileName() {
+		try {
+			$config = new Config();
+			$config->addConfigFile('dfgkhdufhdjhfjk');
+		} catch(\InvalidArgumentException $e) {
+			return;
+		}
+
+		$this->fail('Expected InvalidArgumentException was not thrown.');
+	}
 }
