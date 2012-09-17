8760f7c6d7fd5d73272900cc9fc6ae6dc6f2c1b7
David Henning <madcat.me@gmail.com>
2012-09-17 20:00:46 +0200

    adjusted unit test due to removed singleton in class Encryption

--- tests/MongoAppKit/Tests/EncryptionTest.php
+++ tests/MongoAppKit/Tests/EncryptionTest.php
@@ -7,21 +7,10 @@ use MongoAppKit\Encryption;
 class EncryptionTest extends \PHPUnit_Framework_TestCase {
 
 	public function testEncryption() {
-		$encryption = Encryption::getInstance();
+		$encryption = new Encryption();
 		$value = 'AES Test';
 		$encryptedValue = $encryption->encrypt($value, 'foobar');
 
 		$this->assertEquals($value, $encryption->decrypt($encryptedValue, 'foobar'));
-	}
-
-	public function testClone() {
-		try {
-			$encryption = Encryption::getInstance();
-			$clone = clone $encryption;
-		} catch(\Exception $e) {
-			return;
-		}
-
-		$this->fail('Expected Exception was not thrown.');
 	}
 }
