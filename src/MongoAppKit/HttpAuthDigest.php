82a5bd3cba5b2b6b14bbe69d521614fb768f8339
David Henning <madcat.me@gmail.com>
2012-10-19 23:52:03 +0200

    removed hungarian notation

--- src/MongoAppKit/HttpAuthDigest.php
+++ src/MongoAppKit/HttpAuthDigest.php
@@ -25,60 +25,60 @@ class HttpAuthDigest {
      * @var string
      */
 
-    protected $_sDigest = null;
+    protected $_digestHash = null;
 
     /**
      * Parsed digest array
      * @var array
      */
 
-    protected $_aDigest = null;
+    protected $_digest = null;
 
     /**
      * Realm name
      * @var string
      */
 
-    protected $_sRealm = null;
+    protected $_realm = null;
 
     /**
      * Nonce
      * @var string
      */
 
-    protected $_sNonce = null;
+    protected $_nonce = null;
 
     /**
      * Opaque
      * @var string
      */
 
-    protected $_sOpaque = null;
+    protected $_opaque = null;
 
-    public function __construct(Request $oRequest, $sRealm) {
-        $sDigest = $oRequest->server->get('PHP_AUTH_DIGEST');
-        $sHttpAuth = $oRequest->server->get('REDIRECT_HTTP_AUTHORIZATION');
+    public function __construct(Request $request, $realm) {
+        $digest = $request->server->get('PHP_AUTH_DIGEST');
+        $httpAuth = $request->server->get('REDIRECT_HTTP_AUTHORIZATION');
 
-        if(empty($sDigest) && !empty($sHttpAuth)) {
-            $sDigest = $sHttpAuth;
+        if(empty($digest) && !empty($httpAuth)) {
+            $digest = $httpAuth;
         }
        
-        $this->_sDigest = $sDigest;
-        $this->_sRealm = $sRealm;
+        $this->_digestHash = $digest;
+        $this->_realm = $realm;
 
-        $sIp = $oRequest->getClientIp();
-        $sOpaque = sha1($sRealm.$oRequest->server->get('HTTP_USER_AGENT').$sIp);
+        $ip = $request->getClientIp();
+        $opaque = sha1($realm.$request->server->get('HTTP_USER_AGENT').$ip);
 
-        $this->_sNonce = sha1(uniqid($sIp));
-        $this->_sOpaque = $sOpaque;
+        $this->_nonce = sha1(uniqid($ip));
+        $this->_opaque = $opaque;
     }
 
     protected function _parseDigest() {
-        if(empty($this->_sDigest)) {
+        if(empty($this->_digestHash)) {
             throw new HttpException('Unauthorized', 401);
         }
 
-        $aNecessaryParts = array(
+        $necessaryParts = array(
             "nonce"     => 1,
             "nc"        => 1,
             "cnonce"    => 1,
@@ -88,27 +88,27 @@ class HttpAuthDigest {
             "response"  => 1
         );
 
-        $sNecessaryParts = implode("|", array_keys($aNecessaryParts));
-        $aDigest = array();
+        $necessaryPart = implode("|", array_keys($necessaryParts));
+        $digest = array();
 
-        preg_match_all('@(' . $sNecessaryParts . ')=(?:([\'"])([^\2]+?)\2|([^\s,]+))@', $this->_sDigest, $aMatches, PREG_SET_ORDER);
+        preg_match_all('@(' . $necessaryPart . ')=(?:([\'"])([^\2]+?)\2|([^\s,]+))@', $this->_digestHash, $matches, PREG_SET_ORDER);
 
-        foreach($aMatches as $aMatch) {
-            $aDigest[$aMatch[1]] = $aMatch[3] ? $aMatch[3] : $aMatch[4];
-            unset($aNecessaryParts[$aMatch[1]]);
+        foreach($matches as $match) {
+            $digest[$match[1]] = $match[3] ? $match[3] : $match[4];
+            unset($necessaryParts[$match[1]]);
         }
 
-        if(!empty($aNecessaryParts)) {
+        if(!empty($necessaryParts)) {
             throw new HttpException('Bad Request', 400);
         }
 
-        $this->_aDigest = $aDigest;
+        $this->_digest = $digest;
     }
 
-    public function sendAuthenticationHeader($bForce = false) {
-        if(empty($this->_sDigest) || $bForce === true) {
+    public function sendAuthenticationHeader($force = false) {
+        if(empty($this->_digestHash) || $force === true) {
             $header = array(
-                'WWW-Authenticate' => 'Digest realm="' . $this->_sRealm . '",nonce="' . $this->_sNonce . '",qop="auth",opaque="' . $this->_sOpaque . '"'
+                'WWW-Authenticate' => 'Digest realm="' . $this->_realm . '",nonce="' . $this->_nonce . '",qop="auth",opaque="' . $this->_opaque . '"'
             );
 
             return new Response('Please authenticate', 401, $header);          
@@ -119,26 +119,26 @@ class HttpAuthDigest {
 
     public function getUserName() {
         $this->_parseDigest();
-        return $this->_aDigest['username'];
+        return $this->_digest['username'];
     }
 
-    public function authenticate($sToken) {
+    public function authenticate($token) {
         $this->_parseDigest();
-        $a1 = $sToken; // md5("{$username}:{$realm}:{$password}")
-        $a2 = md5("{$_SERVER['REQUEST_METHOD']}:{$this->_aDigest['uri']}");
+        $a1 = $token; // md5("{$username}:{$realm}:{$password}")
+        $a2 = md5("{$_SERVER['REQUEST_METHOD']}:{$this->_digest['uri']}");
 
         $aValidRepsonse = array(
             $a1,
-            $this->_aDigest["nonce"],
-            $this->_aDigest["nc"],
-            $this->_aDigest["cnonce"],
-            $this->_aDigest["qop"],
+            $this->_digest["nonce"],
+            $this->_digest["nc"],
+            $this->_digest["cnonce"],
+            $this->_digest["qop"],
             $a2
         );
 
-        $sValidRepsonse = md5(implode(':', $aValidRepsonse));
+        $validRepsonse = md5(implode(':', $aValidRepsonse));
 
-        if(($sValidRepsonse === $this->_aDigest["response"]) === false) {
+        if(($validRepsonse === $this->_digest["response"]) === false) {
             $e = new HttpException('Unauthorized', 401);
             $e->setCallingObject($this);
             throw $e;
